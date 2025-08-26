4e7ce653173db58ba44ec636605dd513b4a9471b
Swastik Dey �� <swastik.dey.2003@gmail.com>
2025-08-26 18:36:07 +0530

    add gunicorn

--- project/config.py
+++ project/config.py
@@ -13,7 +13,7 @@ load_dotenv()  # loads variables from .env
 app = Flask(__name__, static_folder="static", template_folder="templates")
 CORS(app)
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")#put something random, put in env var
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Flask-Mail config (use Gmail for example)
